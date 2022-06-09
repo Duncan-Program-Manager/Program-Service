f9741ef0f2216d8abb1203fa07128a1289417fd3
Duncan Schoenmakers <400156@student.fontys.nl>
2022-06-09 18:55:52 +0200

    added rabbitmq for cluster

--- src/main/java/com/dpm/program/cloudstorage/DropboxStorage.java
+++ src/main/java/com/dpm/program/cloudstorage/DropboxStorage.java
@@ -14,7 +14,6 @@ public class DropboxStorage {
     private static final String ACCESS_TOKEN = "sl.BJI2kYRjTORS6EVOjOCzxlwGPy8kKfhASsDflmIxLUlj1cd2gayW21Cc9n6XoBnXZyoAt7cEwetRgubgqLA5e5eqbFmIewFK_CATJAjRZZGrA2ks-K4ex29i5e3pTzkuFIhCcd4";
     private static DbxClientV2 client;
 
-
     public DropboxStorage()
     {
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/duncanschoenmakers@hotmail.com").build();
